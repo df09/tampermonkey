549d945d1b6bd51639291237a67938420b8eb08b
df09 <090dfdf@gmail.com>
2024-12-29 01:08:15 -0500

    tm: migrate to github

--- bravura/bc.js
+++ bravura/bc.js
@@ -2,16 +2,16 @@
 // @name         bc
 // @description  bc
 // @namespace    bc
-// @version      23
+// @version      1
 // @author       anon
 // @match        http://bravura-crm.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=glasscompanyny.com
 // @grant        none
 // @run-at       document-end
-// @require      https://raw.githubusercontent.com/df09/tempermonkey/refs/heads/main/bravura/01_tms.js?v=23
-// @require      https://raw.githubusercontent.com/df09/tempermonkey/refs/heads/main/bravura/02_helpers.js?v=23
-// @require      https://raw.githubusercontent.com/df09/tempermonkey/refs/heads/main/bravura/03_menu.js?v=23
-// @require      https://raw.githubusercontent.com/df09/tempermonkey/refs/heads/main/bravura/11_jobPage.js?v=23
+// @require      https://raw.githubusercontent.com/df09/tempermonkey/refs/heads/main/bravura/01_tms.js
+// @require      https://raw.githubusercontent.com/df09/tempermonkey/refs/heads/main/bravura/02_helpers.js
+// @require      https://raw.githubusercontent.com/df09/tempermonkey/refs/heads/main/bravura/03_menu.js
+// @require      https://raw.githubusercontent.com/df09/tempermonkey/refs/heads/main/bravura/11_jobPage.js
 // ==/UserScript==
 
 (function() {
