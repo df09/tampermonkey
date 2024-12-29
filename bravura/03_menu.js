002999ac0ba6919dc59ff2a7737f182550692ae6
df09 <090dfdf@gmail.com>
2024-12-28 23:42:41 -0500

    tm: migrate to github

--- bravura/03_menu.js
+++ bravura/03_menu.js
@@ -2,13 +2,13 @@
 // @name         bc
 // @description  bc
 // @namespace    bc
-// @version      4
+// @version      5
 // @author       anon
 // @match        http://bravura-crm.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=glasscompanyny.com
 // @grant        none
 // @run-at       document-end
-// @require      https://github.com/df09/tempermonkey/raw/refs/heads/main/bravura/02_helpers.js?v=4
+// @require      https://github.com/df09/tempermonkey/raw/refs/heads/main/bravura/02_helpers.js?v=5
 // ==/UserScript==
 
 (function() {
