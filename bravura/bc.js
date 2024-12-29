82727d3c0ffa274619ed63988edc0f61a4e6ada4
df09 <090dfdf@gmail.com>
2024-12-29 00:52:15 -0500

    tm: migrate to github

--- bravura/bc.js
+++ bravura/bc.js
@@ -2,16 +2,16 @@
 // @name         bc
 // @description  bc
 // @namespace    bc
-// @version      15
+// @version      16
 // @author       anon
 // @match        http://bravura-crm.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=glasscompanyny.com
 // @grant        none
 // @run-at       document-end
-// @require      https://raw.githubusercontent.com/df09/tempermonkey/refs/heads/main/bravura/01_tms.js?v=15
-// @require      https://raw.githubusercontent.com/df09/tempermonkey/refs/heads/main/bravura/02_helpers.js?v=15
-// @require      https://raw.githubusercontent.com/df09/tempermonkey/refs/heads/main/bravura/03_menu.js?v=15
-// @require      https://raw.githubusercontent.com/df09/tempermonkey/refs/heads/main/bravura/11_jobPage.js?v=15
+// @require      https://raw.githubusercontent.com/df09/tempermonkey/refs/heads/main/bravura/01_tms.js?v=16
+// @require      https://raw.githubusercontent.com/df09/tempermonkey/refs/heads/main/bravura/02_helpers.js?v=16
+// @require      https://raw.githubusercontent.com/df09/tempermonkey/refs/heads/main/bravura/03_menu.js?v=16
+// @require      https://raw.githubusercontent.com/df09/tempermonkey/refs/heads/main/bravura/11_jobPage.js?v=16
 // ==/UserScript==
 
 (function() {
@@ -19,6 +19,6 @@
 
   // ==== load ============================================
   window.addEventListener('load', async () => {
-    console.log('bc: loaded.');
+    console.log('16 bc: loaded.');
   });
 })();
