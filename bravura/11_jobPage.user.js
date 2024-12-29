22cfe836861977ec3363b24f99f6b20be2058a20
df09 <090dfdf@gmail.com>
2024-12-28 23:50:04 -0500

    tm: migrate to github

--- bravura/11_jobPage.user.js
+++ bravura/11_jobPage.user.js
@@ -2,15 +2,15 @@
 // @name         bc
 // @description  bc
 // @namespace    bc
-// @version      14
+// @version      1
 // @author       anon
 // @match        http://bravura-crm.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=glasscompanyny.com
 // @grant        none
 // @run-at       document-end
-// @require      https://github.com/df09/tempermonkey/raw/refs/heads/main/bravura/01_tms.js?v=14
-// @require      https://github.com/df09/tempermonkey/raw/refs/heads/main/bravura/02_helpers.js?v=14
-// @require      https://github.com/df09/tempermonkey/raw/refs/heads/main/bravura/03_menu.js?v=14
+// @require      https://raw.githubusercontent.com/df09/tempermonkey/refs/heads/main/bravura/01_tms.user.js?v=1
+// @require      https://raw.githubusercontent.com/df09/tempermonkey/refs/heads/main/bravura/02_helpers.user.js?v=1
+// @require      https://raw.githubusercontent.com/df09/tempermonkey/refs/heads/main/bravura/03_menu.user.js?v=1
 // ==/UserScript==
 
 (function() {
