33603f0f3b6033614a12c730e529081cfeb96a29
df09 <090dfdf@gmail.com>
2025-01-28 18:04:20 -0500

    upd from windows

--- bravura-crm.com/createBarcodes/checkExistingRooms.js
+++ bravura-crm.com/createBarcodes/checkExistingRooms.js
@@ -2,7 +2,7 @@ function createBarcodesCheckExistingRooms() {
   pause(3000);
 
   const data = tmsGet('tm_barcodesData');
-  const rows = document.querySelectorAll('tr[id^="room_"]');
+  const rows = getEls('tr[id^="room_"]');
   const existingRooms = Array.from(rows).map(
     row => row.querySelectorAll('td')[2].textContent.trim()
   );
@@ -21,4 +21,4 @@ function createBarcodesCheckExistingRooms() {
   const fosId = tm fjлsGet('tm_fosId');
   tmsSetOperation('createBarcodes/newProduct');
   redirect(`http://bravura-crm.com/fabrication_orders/${fosId}/new_product`);
-}
+};
